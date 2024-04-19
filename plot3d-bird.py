2a3ea50024f02466cee6b89ca5cb84cf67f58576
Sinamahani <sina.sabermahani@gmail.com>
2024-04-19 13:30:27 -0500

    creating the bash file for plotting the bird-view of rf models

--- plot3d-bird.py
+++ plot3d-bird.py
@@ -1,8 +1,3 @@
-PROJECTNAME="Plot3D of RF models"
-echo $PROJECTNAME
-
-# write the python code
-cat <<EOF > plot3d-bird.py
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
