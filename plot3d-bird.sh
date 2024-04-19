7ab30a76211e496e64f6cf82e5de52c4960f40b5
Sinamahani <sina.sabermahani@gmail.com>
2024-04-19 13:40:55 -0500

    qasd q Please enter the commit message for your changes. Lines starting

--- plot3d-bird.sh
+++ plot3d-bird.sh
@@ -1,9 +1,16 @@
 PROJECTNAME="Plot3D of RF models"
 echo $PROJECTNAME
 
-# write the python code
-cat <<EOF > plot3d-bird.py
-import numpy as np
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-from sklearn.ensemble import RandomForestRegressor
+#step 1
+# reading all station name and the go through each stations
+# /inv/results/{station_name}/flatmoho/*_result.csv
+
+#step 2
+#in each station file, the program read result files and extarct "layers" and "fun"
+
+#step 3
+# plot layers vs fun using python or gnuplot
+
+#step 4
+#ask me a the layer number and the save it in a file with the the format "station_name, layer_number, fun"
+
