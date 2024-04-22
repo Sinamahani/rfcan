2e93bb1c0cb345b8194232883f915cd818d65ac6
Sinamahani <sina.sabermahani@gmail.com>
2024-04-22 17:40:33 -0500

    done with extarcting the optimum number of layers

--- plot3d-bird.sh
+++ plot3d-bird.sh
@@ -2,37 +2,56 @@ PROJECTNAME="Plot3D is running ..."
 echo $PROJECTNAME
 mkdir -p inv/plot3d/bird-view
 
-ls inv/results | while read station;
+function reading_all_files() {
+    ls inv/results | while read station;
     do 
     echo 'layer,fun' > inv/plot3d/bird-view/$station.csv;
     for i in {2..7};
             do awk -F',' 'NR > 1 {print $2,$3}' inv/results/$station/flatmoho/*_$i*result.csv | sed 's/ /,/g' >> inv/plot3d/bird-view/$station.csv;
         done;
     done
+}
 
-# Install gnuplot package if not already installed
-if ! command -v gnuplot &> /dev/null; then
-    sudo apt-get install gnuplot -y
-fi
-
-find inv/plot3d/bird-view -name "*.csv" | while IFS= read -r station; do
-head $station
-    # Use gnuplot to plot the data
-gnuplot -persist <<- EOF
-        set title "Plot for $station"
-        set xlabel "X-axis"
-        set ylabel "Y-axis"
-        set grid
-        set datafile separator ","
-        set xrange [2:7]  # Adjust according to your data's expected range
-        set yrange [GPVAL_DATA_Y_MIN:GPVAL_DATA_Y_MAX]  # Adjust accordingly
-        plot "$station" every ::1 using 1:2 with lines title "Line Plot"
-        set terminal png
-        set output "$station.png"
-
-
+function graph_picker() {
+    find inv/plot3d/bird-view -name "*.csv" | while IFS= read -r station; do
+        echo $station
+        gnuplot -persist <<- EOF
+            set title "Plot for $station"
+            set xlabel "# of Layers"
+            set ylabel "Errors"
+            set grid
+            set datafile separator ","
+            set xrange [2:7]  # Adjust according to your data's expected range
+            # set yrange [GPVAL_DATA_Y_MIN:GPVAL_DATA_Y_MAX]  # Adjust accordingly
+            plot "$station" every ::1 using 1:2 with linespoints 
+            pause mouse keypress
+            x = MOUSE_X
+            set terminal png
+            set output "$station.png"
+            replot
+            set print "$station.tmp"
+            print x
+            unset print
+            
 EOF
-    # Optional: Prompt the user to press enter before continuing with the next file
-read -p "Press enter to continue";
+pkill -f gnuplot
 
 done
+}
+
+function read_tmp_files() {
+    find inv/plot3d/bird-view -name "*.tmp" | while IFS= read -r station; do
+        x=$(cat $station)
+        #extarct the station name
+        stationu=$(echo $station | sed -e 's/inv\/plot3d\/bird-view\///' -e 's/\.csv\.tmp$//')
+        echo $stationu, $x
+        rm $station
+    done
+}
+
+
+# call the functions
+reading_all_files   #reading all layers informtion for each station and extact the error values
+graph_picker    #plot the graph for each station and save the graphs and extract the x value
+read_tmp_files >> inv/plot3d/optimum_num_layers.csv  # read all tmp files and get the x value
+awk -F ',' '{print $1,",", int($2)}' inv/plot3d/optimum_num_layers.csv > inv/plot3d/optimum_num_layers.csv
