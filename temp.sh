284e279cfe2c17488939679d0783bdf1daed8df0
Sinamahani <sina.sabermahani@gmail.com>
2024-04-25 14:28:23 -0500

    fixed transfering

--- temp.sh
+++ temp.sh
@@ -1,13 +1,16 @@
-PROJECTNAME="Plot3D is running ..."
-echo $PROJECTNAME
+#Running the functions for the 3D plot
+source codes_RF/plot3d-func.sh
 
-source plot3d-bird.sh
-# call the functions
+#making backup
+cp -r inv/plot3d inv/plot3d-backup
+rm -r inv/plot3d
+mkdir inv/plot3d
+
+#call the functions
 mkdir -p inv/plot3d/bird-view
 reading_all_files   #reading all layers informtion for each station and extact the error values
-station_list_lat_lon
 graph_picker    #plot the graph for each station and save the graphs and extract the x value
 read_tmp_files >> inv/plot3d/optimum_num_layers.csv  # read all tmp files and get the x value
-awk -F ',' '{print $1,",", int($2)}' inv/plot3d/optimum_num_layers.csv > inv/plot3d/optimum_num_layers.csv
-# rm inv/plot3d/bird-view/*.csv
+rm inv/plot3d/bird-view/*.csv
+station_list_lat_lon
 cp_model_files
