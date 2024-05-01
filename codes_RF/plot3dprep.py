a2b8003e91cc5601fb424baf641bd50325213431
sinamahani <sina.sabermahani@gmail.com>
2024-04-30 20:43:34 -0500

    working on plot3d

--- codes_RF/plot3dprep.py
+++ codes_RF/plot3dprep.py
@@ -8,6 +8,7 @@ Description: for plotting 3d, we need to have depth column in the csv files.
 import pandas as pd
 import os
 import asyncio
+import numpy as np
 
 async def add_depth_column(file_name):
     df = pd.read_csv(file_name)
@@ -27,4 +28,29 @@ async def add_depth_column_to_all_files(file_list):
 if __name__ == "__main__":
     file_list = os.listdir("inv/plot3d/flatmoho/bird-view/")
     file_list = [f"inv/plot3d/flatmoho/bird-view/{f}" for f in file_list if f.endswith(".csv")]
-    asyncio.run(add_depth_column_to_all_files(file_list))
+#    asyncio.run(add_depth_column_to_all_files(file_list))
+
+
+    def make_file_for_plot(file_name: str, depth_points: int=80, **kwargs) -> None:
+        if not isinstance(depth_points, int):
+            raise ValueError("depth_points should be an integer.")
+        if not isinstance(file_name, str):
+            raise ValueError("file_name should be a string.")
+        max_depth = kwargs.get("max_depth", depth_points)
+        
+        # lat, lon, depth, vp, vs, rho, ani
+        df = pd.DataFrame(columns=["lat", "lon", "depth", "vp", "vs", "rho", "ani"])
+        file_name_list = file_name.split("/")[-1].split(".csv")[0].split("%")
+        sta_code = file_name_list[0]
+        layers = file_name_list[1]
+        lat = file_name_list[2]
+        lon = file_name_list[3]
+        depth = np.linspace(0, max_depth, depth_points)
+        for i in depth:
+            df = df.append({"lat": lat, "lon": lon, "depth": i, "vp": 0, "vs": 0, "rho": 0, "ani": 0}, ignore_index=True)
+        print(df.head(10))
+            
+        
+    make_file_for_plot(file_list[0], 80)
+    
+    
